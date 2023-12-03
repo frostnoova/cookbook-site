184904b98e0533c4ed05feec3be7a9f13328f491
frost <logvinov@mail.iszf.irk.ru>
2023-12-03 22:35:50 +0800

    fix name fileds, added migrations

--- recipes/models.py
+++ recipes/models.py
@@ -3,19 +3,19 @@ from django.db import models
 
 class Product(models.Model):
 
-    name = models.CharField('Name Product', max_length=100, db_index=True)
-    quantity_cooked = models.PositiveSmallIntegerField(default=0)
+    name = models.CharField('Название продукта', max_length=100, db_index=True)
+    quantity_cooked = models.PositiveSmallIntegerField('Приготовлено блюд',default=0)
 
     class Meta:
         ordering = ('name', )
 
     def __str__(self):
-        return f'{self.name}: quantity cooked {self.quantity_cooked}'
+        return f'{self.name}'
 
 
 class Recipe(models.Model):
 
-    name = models.CharField('Name Recipe', max_length=100, db_index=True)
+    name = models.CharField('Название рецепта', max_length=100, db_index=True, unique=True)
     products = models.ManyToManyField(Product, through='RecipeDescription')
 
     def __str__(self):
@@ -26,15 +26,17 @@ class RecipeDescription(models.Model):
 
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='description')
-    weight = models.PositiveSmallIntegerField('Weight in grams')
+    weight = models.PositiveSmallIntegerField('Вес в граммах')
     
 
     class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=('product', 'recipe'),
-                name='unique_recipe_description')
-        ]
+        # constraints = [
+        #     models.UniqueConstraint(
+        #         fields=('product', 'recipe'),
+        #         name='unique_recipe_description')
+        # ]
+
+        ordering = ('recipe_id', )
 
     def __str__(self):
-        return f'{self.recipe.name} ({self.product.name} {self.weight} grams)'
+        return f'{self.product.name} {self.weight} г'
