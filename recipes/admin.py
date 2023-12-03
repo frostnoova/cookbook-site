f1f75300c8712fe2d3abb0b4cfa5cc2cd4c67ecb
frost <logvinov@mail.iszf.irk.ru>
2023-12-03 22:36:48 +0800

    register models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from .models import Recipe, Product, RecipeDescription
 # Register your models here.
+
+
+class Description(admin.StackedInline):
+    model = RecipeDescription
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = (Description, )
+    list_display = ['id', 'name', 'get_products']
+    list_display_links = ['name']
+
+    @admin.display(description="Продукты")
+    def get_products(self, instance):
+        return [product for product in instance.products.all()]
+
+
+@admin.register(RecipeDescription)
+class RecipeDescriptionAdmin(admin.ModelAdmin):
+    list_display = ['id', 'product', 'product_id', 'recipe', 'recipe_id','weight']
+    list_display_links = ['id']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'quantity_cooked']
+    list_display_links = ['name']
