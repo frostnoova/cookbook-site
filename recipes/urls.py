382e1f0eeedaa141f4a6a14b3f3e84c977d668fa
frost <logvinov@mail.iszf.irk.ru>
2023-12-04 18:03:34 +0800

    update path, and function

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from recipes import views
 
 urlpatterns = [
-    path("", views.recipes, name='recipes'),
-    path("products/", views.products, name='products')
+    path("add_product/", views.add_product, name='add_product'),
+    path("show_recipes/", views.show_recipes, name='show_recipes'),
+    path("cook_products/", views.cook_products, name='cook_products')
 ]
